a080736a5e7546aae3e7ed1db4da86a158838e0d
Shankhadeep <shankhadeep17.sb@gmail.com>
2025-08-02 16:38:41 +0530

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-3xl font-general'>
+      App
+      <h1 className='font-circular text-violet-300'>This is new font</h1>
+    </div>
+  )
+}
+
+export default App
