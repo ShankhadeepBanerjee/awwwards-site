c2133a04e011e29e59edd0d0d3eab4da0de33758
Shankhadeep <shankhadeep17.sb@gmail.com>
2025-08-03 16:54:30 +0530

    feat: add Prettier configuration file

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+export default {
+    "singleQuote": true,
+    "trailingComma": "es5",
+    "tabWidth": 2,
+    "semi": true,
+    "printWidth": 80,
+    "arrowParens": "always",
+    "bracketSpacing": true,
+    "jsxBracketSameLine": false,
+    "useTabs": false,
+    "plugins": ["prettier-plugin-jsx"],
+    "jsxSingleQuote": false
+}
