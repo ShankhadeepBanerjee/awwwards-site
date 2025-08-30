eb4441f1d62076f8dc651d1ec838bc07bd8a827a
Shankhadeep <shankhadeep17.sb@gmail.com>
2025-08-30 13:59:08 +0530

    refactor: clean up About component and improve text content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,12 @@
-import React from "react";
-
+import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
 import AnimatedTitle from "./AnimatedTitle";
 
 gsap.registerPlugin(ScrollTrigger);
 
-function About() {
+const About = () => {
   useGSAP(() => {
     const clipAnimation = gsap.timeline({
       scrollTrigger: {
@@ -19,19 +18,20 @@ function About() {
         pinSpacing: true,
       },
     });
+
     clipAnimation.to(".mask-clip-path", {
       width: "100vw",
       height: "100vh",
       borderRadius: 0,
     });
-  }, []);
+  });
 
   return (
     <div id="about" className="min-h-screen w-screen">
       <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
-        <h2 className="font-general text-sm uppercase md:text-[10px]">
+        <p className="font-general text-sm uppercase md:text-[10px]">
           Welcome to Zentry
-        </h2>
+        </p>
 
         <AnimatedTitle
           title="Disc<b>o</b>ver the world's <br /> largest shared <b>a</b>dventure"
@@ -39,21 +39,25 @@ function About() {
         />
 
         <div className="about-subtext">
-          <p>The Game of Games begin-your life, now an epic MMORPG</p>
-          <p>Zentry unites every player from countless games and platforms</p>
+          <p>The Game of Games begins—your life, now an epic MMORPG</p>
+          <p className="text-gray-500">
+            Zentry unites every player from countless games and platforms, both
+            digital and physical, into a unified Play Economy
+          </p>
         </div>
       </div>
+
       <div className="h-dvh w-screen" id="clip">
         <div className="mask-clip-path about-image">
           <img
             src="img/about.webp"
-            alt="Backgorund"
+            alt="Background"
             className="absolute left-0 top-0 size-full object-cover"
           />
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default About;
